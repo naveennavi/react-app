57b73935861872b8d048348206643432c75550f5
naveennavi <naveenj085@gmail.com>
2019-03-08 19:58:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,24 +2,27 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import SignUpForm from './pages/SignUpForm';
 import SignInForm from './pages/SignInForm';
+import Dashboard from './pages/Dashboard';
 import './App.css';
 
+
 class App extends Component {
+  
   render() {
     return (
       <Router>
       <div className="App">
-      <div className="App__Aside"></div>
-      <div className="App__Form">
+      
       
 
       <Route exact path = "/" component = { SignInForm }>
-      
       </Route>
       <Route path ="/sign-up" component = { SignUpForm }>
       </Route>
+      <Route path ="/dashboard" component = { Dashboard }>
+      </Route>
       </div>
-      </div>
+      
       </Router>
     );
   }
